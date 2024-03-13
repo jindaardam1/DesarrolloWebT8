e9b5c143dd4de39ffe60a0c1241a056ec01e01e7
mpuentebdam1 <mpuentebdam1@gmail.com>
2024-03-13 10:49:11 +0100

    .gitignore update

---
+++ ActividadEvaluableUT8MVC/ActividadEvaluableUT8MVC/Controllers/Controlador.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadEvaluableUT8MVC.Controllers
+{
+    public class Controlador : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome()
+        {
+            return "Este es el método de bienvenida";
+        }
+    }
+}
