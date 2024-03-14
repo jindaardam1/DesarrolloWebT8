0039cc78344c407d48f817dd0ad952029d09c0c8
Jagoba Inda <jindaardam1@gmail.com>
2024-03-14 21:46:34 +0100

    Script ids autoincrementales

--- script_creacion_db.sql
+++ script_creacion_db.sql
@@ -31,34 +31,34 @@ CREATE TABLE masones (
 
 
 -- Inserciones de ejemplo en la tabla "logias"
-INSERT INTO logias (logia_id, name, description) VALUES 
-(1, 'Logia de los Elementales', 'Logia que agrupa a criaturas relacionadas con los elementos naturales'),
-(2, 'Logia de las Sombras', 'Logia que reúne a criaturas nocturnas y oscuras'),
-(3, 'Logia de Ibai', 'Logia que venera al ser supremo Ibai'),
-(4, 'Logia de los Espíritus', 'Logia que estudia y convoca espíritus ancestrales');
+INSERT INTO logias (name, description) VALUES 
+('Logia de los Elementales', 'Logia que agrupa a criaturas relacionadas con los elementos naturales'),
+('Logia de las Sombras', 'Logia que reúne a criaturas nocturnas y oscuras'),
+('Logia de Ibai', 'Logia que venera al ser supremo Ibai'),
+('Logia de los Espíritus', 'Logia que estudia y convoca espíritus ancestrales');
 
 -- Inserciones de ejemplo en la tabla "creatures"
-INSERT INTO creatures (creature_id, name, description, logia_id) VALUES 
-(1, 'Cerbero', 'Perro de tres cabezas guardián del inframundo', 1),
-(2, 'Minotauro', 'Criatura con cuerpo de humano y cabeza de toro', 1),
-(3, 'Hidra', 'Serpiente acuática con múltiples cabezas', 1),
-(4, 'Quimera', 'Criatura con cabeza de león, cuerpo de cabra y cola de serpiente', 3),
-(5, 'Vampiro', 'Criatura que se alimenta de la sangre de los vivos', 2),
-(6, 'Licántropo', 'Humano que se transforma en lobo en las noches de luna llena', 2),
-(7, 'Súcubo', 'Demonio femenino que seduce a los hombres en sueños', 4),
-(8, 'Kraken', 'Monstruo marino de enormes proporciones', 3),
-(9, 'Fénix', 'Ave legendaria que renace de sus cenizas', 1),
-(10, 'Grifo', 'Criatura con cuerpo de león y cabeza de águila', 1);
+INSERT INTO creatures (name, description, logia_id) VALUES 
+('Cerbero', 'Perro de tres cabezas guardián del inframundo', 1),
+('Minotauro', 'Criatura con cuerpo de humano y cabeza de toro', 1),
+('Hidra', 'Serpiente acuática con múltiples cabezas', 1),
+('Quimera', 'Criatura con cabeza de león, cuerpo de cabra y cola de serpiente', 3),
+('Vampiro', 'Criatura que se alimenta de la sangre de los vivos', 2),
+('Licántropo', 'Humano que se transforma en lobo en las noches de luna llena', 2),
+('Súcubo', 'Demonio femenino que seduce a los hombres en sueños', 4),
+('Kraken', 'Monstruo marino de enormes proporciones', 3),
+('Fénix', 'Ave legendaria que renace de sus cenizas', 1),
+('Grifo', 'Criatura con cuerpo de león y cabeza de águila', 1);
 
 -- Inserciones de ejemplo en la tabla "masones"
-INSERT INTO masones (masone_id, name, description, logia_id) VALUES 
-(1, 'John Doe', 'Mason perteneciente a la Logia de los Elementales', 3),
-(2, 'Jane Smith', 'Mason perteneciente a la Logia de las Sombras', 2),
-(3, 'Michael Johnson', 'Mason perteneciente a la Logia de los Elementales', 1),
-(4, 'Emily Brown', 'Mason perteneciente a la Logia de las Sombras', 2),
-(5, 'David Williams', 'Mason perteneciente a la Logia de los Elementales', 1),
-(6, 'Emma Jones', 'Mason perteneciente a la Logia de las Sombras', 4),
-(7, 'James Miller', 'Mason perteneciente a la Logia de los Elementales', 3),
-(8, 'Olivia Davis', 'Mason perteneciente a la Logia de las Sombras', 2),
-(9, 'William Wilson', 'Mason perteneciente a la Logia de los Elementales', 1),
-(10, 'Sophia Taylor', 'Mason perteneciente a la Logia de las Sombras', 3);
+INSERT INTO masones (name, description, logia_id) VALUES 
+('John Doe', 'Mason perteneciente a la Logia de los Elementales', 3),
+('Jane Smith', 'Mason perteneciente a la Logia de las Sombras', 2),
+('Michael Johnson', 'Mason perteneciente a la Logia de los Elementales', 1),
+('Emily Brown', 'Mason perteneciente a la Logia de las Sombras', 2),
+('David Williams', 'Mason perteneciente a la Logia de los Elementales', 1),
+('Emma Jones', 'Mason perteneciente a la Logia de las Sombras', 4),
+('James Miller', 'Mason perteneciente a la Logia de los Elementales', 3),
+('Olivia Davis', 'Mason perteneciente a la Logia de las Sombras', 2),
+('William Wilson', 'Mason perteneciente a la Logia de los Elementales', 1),
+('Sophia Taylor', 'Mason perteneciente a la Logia de las Sombras', 3);
